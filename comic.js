b4f36456caadfd20b4933b44c604ad872ee91ac5
Harri <harkiel89@gmail.com>
2023-10-14 23:09:27 +0300

    Modify comic.js, function getComic() now returns image source url for comic

--- comic.js
+++ comic.js
@@ -1,13 +1,18 @@
 const url = "https://c.xkcd.com/random/comic/";
 import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
 
 async function getComic() {
   const response = await fetch(url);
   let comicUrl = response.url;
-  console.log(comicUrl);
+
   const comicPage = await fetch(comicUrl);
   const html = await comicPage.text();
-  return html;
+
+  const dom = new JSDOM(html);
+  let myContent = dom.window.document.querySelector("#comic img").src;
+
+  return myContent;
 }
 
 getComic().then((data) => console.log(data));
