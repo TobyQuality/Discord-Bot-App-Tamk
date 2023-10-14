b8a8547f8a9ec2ed4c36d43663ca4f5015326bdf
Harri <harkiel89@gmail.com>
2023-10-14 22:18:48 +0300

    Modify comic.js, add function getComic()

--- comic.js
+++ comic.js
@@ -1,1 +1,9 @@
 const url = "https://c.xkcd.com/random/comic/";
+import fetch from "node-fetch";
+
+async function getComic() {
+  const response = await fetch(url);
+  return response;
+}
+
+getComic().then((data) => console.log(data));
