6537ca4935093215bb91b6a23bb26fb2eb86eabb
Roosa Pasanen <Roosa Pasanen>
2023-10-25 15:28:40 +0300

    tictactoe.js: (create2DArray): Update Javadoc

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,8 @@
-// Creates a specified size 2d array and populates it with a specific character
+/** Creates a specified size 2d array and populates it with a specific character
+    @param size - determines the size of the array
+    @param chara - determines the character the array is populated with
+    @return - returns the array
+*/
 export function createTTTArray(size, chara) {
     let tttArray = [];
     for (let i = 0; i < size; i++) {
@@ -8,4 +12,5 @@ export function createTTTArray(size, chara) {
         }
         tttArray[i] = tttSubArray;
     }
+    return tttArray;
 }
