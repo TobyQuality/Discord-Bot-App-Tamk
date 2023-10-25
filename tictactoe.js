4d5e8398bcdb23ffc7691d8c5da5d641e7458d66
Roosa Pasanen <Roosa Pasanen>
2023-10-25 15:20:07 +0300

    tictactoe.js: (create2DArray): Add functionality to populate the array with character of your choice

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,10 @@
 // Creates a specified size 2d array and populates it with a specific character
-export function createTTTArray(size) {
+export function createTTTArray(size, chara) {
     let tttArray = [];
     for (let i = 0; i < size; i++) {
         let tttSubArray = [];
         for (let j = 0; j < size; j++) {
-            tttSubArray[j] = "⬛";
+            tttSubArray[j] = chara;
         }
         tttArray[i] = tttSubArray;
     }
