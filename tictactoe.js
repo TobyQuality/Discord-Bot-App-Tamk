c9bba4f0cfe2afbe66dcc3ba26ab75cd0c9b752e
Roosa Pasanen <Roosa Pasanen>
2023-10-25 15:15:24 +0300

    tictactoe.js: (create2DArray): Add functionality.

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,10 @@
 export function createTTTArray(size) {
-
+    let tttArray = [];
+    for (let i = 0; i < size; i++) {
+        let tttSubArray = [];
+        for (let j = 0; j < size; j++) {
+            tttSubArray[j] = "⬛";
+        }
+        tttArray[i] = tttSubArray;
+    }
 }
