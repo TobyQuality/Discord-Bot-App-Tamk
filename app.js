8f1b21529aca900aa6a54ad8e51cf7ce309f812c
Harri Salminen <harri.salminen@tuni.fi>
2023-11-19 21:31:28 +0200

    Remove author from comic embed, use setURL instead

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post("/interactions", async function (req, res) {
       const embed = new EmbedBuilder()
         .setColor(0x0099ff)
         .setTitle(comic.title)
-        .setAuthor({ name: comic.url, url: comic.url })
+        .setURL(comic.url)
         .setImage(comic.image)
         .setTimestamp();
       // Send a message into the channel where command was triggered from
