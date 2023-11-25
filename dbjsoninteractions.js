c17e7f9569acbe29f3f4200c66cf59d0c9d28ecc
Toby Quality <topias.laatu@gmail.com>
2023-11-25 20:54:06 +0200

    dbjsoninteractions.js: Add a function to create a new message to db.json

--- dbjsoninteractions.js
+++ dbjsoninteractions.js
@@ -7,3 +7,9 @@ export async function getMessages() {
   const res = await axios.get("http://localhost:3000/api/messages");
   return res.data;
 }
+
+// create a function to create a new message
+export async function createMessage(message) {
+  const res = await axios.post("http://localhost:3000/api/messages", message);
+  return res.data;
+}
