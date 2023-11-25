6f78b099ca11c438e203d2294884824b856d1bd9
Toby Quality <topias.laatu@gmail.com>
2023-11-25 20:49:49 +0200

    dbjsoninteractions.js: Add a function to get messages stored in db.json

--- dbjsoninteractions.js
+++ dbjsoninteractions.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
 // create functions to make API calls
+
+// create a function to get all posts
+export async function getMessages() {
+  const res = await axios.get("http://localhost:3000/api/messages");
+  return res.data;
+}
