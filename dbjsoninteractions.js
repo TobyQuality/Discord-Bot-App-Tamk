0f722e7d31ccb4cf0eba9bd7260dbeace970ae7d
Toby Quality <topias.laatu@gmail.com>
2023-11-27 11:58:51 +0200

    dbjsoninteractions: change url to correct form

--- dbjsoninteractions.js
+++ dbjsoninteractions.js
@@ -4,12 +4,12 @@ import axios from "axios";
 
 // create a function to get all posts
 export async function getMessages() {
-  const res = await axios.get("http://localhost:3000/api/messages");
+  const res = await axios.get("http://localhost:3000/messages");
   return res.data;
 }
 
 // create a function to create a new message
 export async function createMessage(message) {
-  const res = await axios.post("http://localhost:3000/api/messages", message);
+  const res = await axios.post("http://localhost:3000/messages", message);
   return res.data;
 }
