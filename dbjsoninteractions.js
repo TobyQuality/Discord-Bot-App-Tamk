5f98368ef24180f2926e00932cf942cd1a494468
Toby Quality <topias.laatu@gmail.com>
2023-11-27 12:17:47 +0200

    dbjsoninteractions: chane the port address to 4000

--- dbjsoninteractions.js
+++ dbjsoninteractions.js
@@ -2,14 +2,16 @@ import axios from "axios";
 
 // create functions to make API calls
 
+const url = "http://localhost:4000/messages";
+
 // create a function to get all posts
 export async function getMessages() {
-  const res = await axios.get("http://localhost:3000/messages");
+  const res = await axios.get(url);
   return res.data;
 }
 
 // create a function to create a new message
 export async function createMessage(message) {
-  const res = await axios.post("http://localhost:3000/messages", message);
+  const res = await axios.post(url, message);
   return res.data;
 }
