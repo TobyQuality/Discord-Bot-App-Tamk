5c4b9db84e89ae6de8cc0122f58856055bb2f987
Toby Quality <topias.laatu@gmail.com>
2023-11-28 09:32:05 +0200

    bot.js(showMessages()): Move the json server interactions to bot.js, starting with showMessages function

--- bot.js
+++ bot.js
@@ -3,3 +3,14 @@ const axios = require("axios");
 
 const client = new Discord.Client();
 const prefix = "/"; // Command prefix
+
+// Function to fetch messages from db.json
+async function showMessages() {
+  try {
+    const response = await axios.get("http://localhost:4000/messages"); // Replace with your JSON server URL
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching messages:", error);
+    return "Error fetching messages";
+  }
+}
