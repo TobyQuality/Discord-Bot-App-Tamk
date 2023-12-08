a2f6a5e416e6f6a9b801a74e021b43a308b1df0d
Toby Quality <topias.laatu@gmail.com>
2023-12-08 20:38:35 +0200

    starwars.js: Get necessary imports

--- starwars.js
+++ starwars.js
@@ -1,5 +1,7 @@
 // create a function for fetching star wars information from the star wars api: https://swapi.dev/api/
 
+import { fetchUrl } from "./utils.js";
+
 const url = "https://swapi.dev/api/";
 
 export async function getStarWarsInfo() {}
