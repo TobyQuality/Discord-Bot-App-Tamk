c7086f1131f76e286e9eb1e2b9925ca0b705865b
Toby Quality <topias.laatu@gmail.com>
2023-12-08 20:37:39 +0200

    starwars.js: Initialize an asynchronous function called getStarWarsInfo

--- starwars.js
+++ starwars.js
@@ -1,3 +1,5 @@
 // create a function for fetching star wars information from the star wars api: https://swapi.dev/api/
 
 const url = "https://swapi.dev/api/";
+
+export async function getStarWarsInfo() {}
