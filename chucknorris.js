948d179a9a20782603dd53d8825285c898093301
Toby Quality <topias.laatu@gmail.com>
2023-12-08 17:08:37 +0200

    everything ready for merge with devel

--- chucknorris.js
+++ chucknorris.js
@@ -1,14 +1,13 @@
 // create an api for getting a random chuck norris joke
 
+const url = "https://api.chucknorris.io/jokes/random";
+import { fetchUrl } from "./utils.js";
+
 /**
  * Fetches a random Chuck Norris joke from the API
  * @returns {string} joke
  * @see https://api.chucknorris.io/jokes/random
  */
-
-const url = "https://api.chucknorris.io/jokes/random";
-import { JSDOM } from "jsdom";
-import { fetchUrl } from "./utils.js";
 
 export async function getChuckNorrisJoke() {
   const response = await fetchUrl(url);
