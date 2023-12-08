6c6808b2a82f508b05ac219952b0cc1d5e7baf1e
Toby Quality <topias.laatu@gmail.com>
2023-12-08 13:32:14 +0200

    chucknorris.js: Use relevant imports

---
+++ chucknorris.js
@@ -0,0 +1,5 @@
+// create an api for getting a random chuck norris joke
+
+const url = "https://api.chucknorris.io/jokes/random";
+import { JSDOM } from "jsdom";
+import { fetchUrl } from "./utils.js";
