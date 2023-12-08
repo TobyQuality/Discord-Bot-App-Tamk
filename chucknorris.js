6e4a20da27db418c95519a89711553401a73f1a2
Toby Quality <topias.laatu@gmail.com>
2023-12-08 14:23:09 +0200

    chucknorris.js: add commentary

--- chucknorris.js
+++ chucknorris.js
@@ -1,5 +1,11 @@
 // create an api for getting a random chuck norris joke
 
+/**
+ * Fetches a random Chuck Norris joke from the API
+ * @returns {string} joke
+ * @see https://api.chucknorris.io/jokes/random
+ */
+
 const url = "https://api.chucknorris.io/jokes/random";
 import { JSDOM } from "jsdom";
 import { fetchUrl } from "./utils.js";
