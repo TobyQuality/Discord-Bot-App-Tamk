566acb78893ece0fc653f33136d004a706b082c5
Toby Quality <topias.laatu@gmail.com>
2023-12-08 20:40:57 +0200

    starwars.js: Add try catch for the function.

--- starwars.js
+++ starwars.js
@@ -5,9 +5,13 @@ import { fetchUrl } from "./utils.js";
 const url = "https://swapi.dev/api/";
 
 export async function getStarWarsInfo() {
-  const response = await fetchUrl(url);
-  const json = await response.json();
-  const info = json;
+  try {
+    const response = await fetchUrl(url);
+    const json = await response.json();
+    const info = json;
+  } catch (error) {
+    throw new Error(`Failed to fetch data: ${error.message}`);
+  }
 
   return info;
 }
